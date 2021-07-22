621e3f43d9f7fd99026b2aa8630c15f248f303ca
imyhacker <arifarhan1602@gmail.com>
2021-07-22 13:45:08 +0700

    Update Template

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="footer text-center bg-blue text-light py-2">
+        asd
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home container mt-5">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+<div>
+  <div class="home mt-5">
+    <div class="jumbotron text-center">
+      <img src="../assets/pp.jpeg" class="rounded-circle img-thumbnail shadow-lg" width="200">
+      <h1 class="display-4">Ari Farhan</h1>
+      <p class="lead">
+        "Just ordinary people who like technology"
+      </p>
+      <p>
+        WebDev | Content Creator | Null
+      </p>
+     
+      <a class="btn btn-primary btn-sm cus-btn" href="#" role="button">Learn more</a>
+    </div>
+  </div>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#deb887" fill-opacity="1" d="M0,160L48,138.7C96,117,192,75,288,90.7C384,107,480,181,576,202.7C672,224,768,192,864,165.3C960,139,1056,117,1152,112C1248,107,1344,117,1392,122.7L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  name: "Home",
 };
 </script>
+
+<style>
+.jumbotron {
+  padding-top: 2rem;
+  background-color:burlywood;
+}
+.cus-btn{
+  border-radius: 10px 10px 10px 0.1px;
+}
+</style>
